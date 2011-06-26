09672a4a39a5d3fc4b7e400083402e846d99f15b
Rodrigo Lazoti <rodrigolazoti@gmail.com>
2011-06-26 00:16:56 -0300

    Personal files added

---
+++ bash_profile
@@ -0,0 +1,7 @@
+export TERM=xterm-color
+export GREP_OPTIONS="--color=auto"
+export GREP_COLOR="4;33"
+export CLICOLOR="auto"
+export PROMPT_COMMAND='echo -ne "\033]0;${PWD}\007"'
+
+source ~/.bash/enviroment
---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cp -f bash_profile ~/.bash_profile
+cp -f gitconfig ~/.gitconfig
+cp -f profile ~/.profile
+cp -f git_completion.sh ~/.git_completion.sh
+
+rm -rf ~/.bash
+mkdir ~/.bash
+
+cp -f bash/PS1 ~/.bash/PS1
+cp -f bash/aliases ~/.bash/aliases
+cp -f bash/colors ~/.bash/colors
+cp -f bash/enviroment ~/.bash/enviroment
+cp -f bash/home ~/.bash/home
+cp -f bash/paths ~/.bash/paths
+cp -f bash/rvm ~/.bash/rvm
+
+echo "Files installed"
+
+source ~/.bash_profile
+echo "Bash profile reloaded"
